b1105ae4f608ffb5978ee69de3de93851db56af7
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-08-24 12:59:26 +0200

    update default build netlify

--- src/app/auth/logout/page.js
+++ src/app/auth/logout/page.js
@@ -1,16 +1,16 @@
 // app/auth/logout/page.js
 
-import { signOut } from 'next-auth/react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { signOut } from 'next-auth/react';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
 
-export default function Logout() {
-    const router = useRouter();
+// export default function Logout() {
+//     const router = useRouter();
 
-    useEffect(() => {
-        // Déconnexion de l'utilisateur
-        signOut({ callbackUrl: '/unauthorized' });
-    }, [router]);
+//     useEffect(() => {
+//         // Déconnexion de l'utilisateur
+//         signOut({ callbackUrl: '/unauthorized' });
+//     }, [router]);
 
-    return <div>Déconnexion en cours...</div>;
-}
+//     return <div>Déconnexion en cours...</div>;
+// }
