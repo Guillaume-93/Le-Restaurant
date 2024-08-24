74a3fb2b57d9a7fd624e191b67e90934cd21e1ee
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-08-24 13:56:48 +0200

    Add favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,16 +8,27 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./globals.css";
 import { usePathname } from 'next/navigation';
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
-  // Si le chemin commence par "/admin", n'utilisez pas Header et Footer
+  // Si le chemin commence par "/admin" ou "/auth", n'utilisez pas Header et Footer
   const isAdminRoute = pathname.startsWith('/admin');
   const isAuthRoute = pathname.startsWith('/auth');
 
   return (
     <html lang="fr">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <title>Mon Application</title>
+      </Head>
       <body className="min-h-screen flex flex-col bg-white">
         <SessionProvider>
           {!isAdminRoute && !isAuthRoute && <Header />}
