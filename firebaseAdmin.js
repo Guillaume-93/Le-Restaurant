02683aa8a48b51ae74d5363a0ed0efbcd364942e
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-08-27 21:50:29 +0200

    comment serviceAccount

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,7 @@ import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
-import serviceAccount from './keys/serviceAccountKey.json';
+// import serviceAccount from './keys/serviceAccountKey.json';
 
 let adminApp;
 
