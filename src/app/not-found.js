8d8fbc2c96f68ce20fc9919583a30f035d9e4ce0
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-08-28 10:33:32 +0200

    &apos; not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -11,13 +11,13 @@ export default function Custom404() {
                 <Image className='sm:h-32 w-auto' src="/images/logos/le-neuilly-canva.png" alt="Le Neuilly Logo" width={1097} height={845} />
                 <h1 className="text-4xl font-bold text-gray-800 mt-8">Oops! Cette page est introuvable...</h1>
                 <p className="text-lg text-gray-600 mt-4">
-                    On dirait que vous avez commandé un plat qui n'est pas au menu.
+                    On dirait que vous avez commandé un plat qui n&apos;est pas au menu.
                 </p>
                 <Link
                     href={'/'}
                     className="w-full bg-gray-300 text-gray-800 py-2 px-4 rounded-md shadow hover:bg-gray-400 text-center"
                 >
-                    Retour à l'accueil
+                    Retour à l&apos;accueil
                 </Link>
             </div>
         </div>
