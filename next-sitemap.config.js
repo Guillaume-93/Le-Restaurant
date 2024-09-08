3bbad6fd1568ecce71c5fe4b9f3d435c00a93ff4
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-09-08 18:30:47 +0200

    SEO sitemap/robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://le-neuilly.netlify.app', // L'URL de base de votre site
+    generateRobotsTxt: true, // Génère un fichier robots.txt automatiquement
+    sitemapSize: 7000, // Limite de pages par fichier sitemap
+    changefreq: 'daily', // Fréquence de mise à jour
+    priority: 0.7, // Priorité par défaut des pages
+    generateIndexSitemap: true, // Génère un index des sitemaps
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                disallow: '/admin', // Bloque toutes les routes commençant par /admin
+            },
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Bingbot',
+                allow: '/',
+            },
+        ],
+        additionalSitemaps: [
+            'https://le-neuilly.netlify.app/sitemap.xml', // Lien vers votre sitemap principal
+        ],
+    },
+};
