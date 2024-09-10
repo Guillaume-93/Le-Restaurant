0394a5419f1f5746df70b2a230c352870d78e238
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-09-10 08:42:58 +0200

    commit

--- src/app/404.js
+++ src/app/404.js
@@ -8,10 +8,10 @@ export default function NotFound() {
             <div className="flex flex-col items-center justify-center p-4 lg:pl-24 space-y-6">
                 <h1 className="text-7xl md:text-[8rem] xl:text-[14rem] font-bold text-gray-800 text-center w-full mb-64 sm:mb-0">404</h1>
                 <p className="text-lg text-gray-900 max-w-sm text-center">
-                    Il semble que vous avez saisi une URL incorrecte. La page que vous cherchez n'existe pas.
+                    Il semble que vous avez saisi une URL incorrecte. La page que vous cherchez n&apos;existe pas.
                 </p>
                 <Link href="/" className="text-black py-2 shadow-default bg-transparent hover:bg-white hover:bg-opacity-10 p-10 rounded-lg backdrop-blur-md ring-1 ring-black">
-                    Retour à la page d'accueil
+                    Retour à la page d&apos;accueil
                 </Link>
             </div>
         </div>
