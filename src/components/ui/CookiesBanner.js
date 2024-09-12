aebe314abccd4d3c9eed15b2c11907505704f689
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-09-12 09:32:39 +0200

    CookiesBanners "En développement"

--- src/components/ui/CookiesBanner.js
+++ src/components/ui/CookiesBanner.js
@@ -30,8 +30,8 @@ export default function CookieBanner() {
     return (
         <>
             <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:bottom-1 sm:px-6 sm:pb-2 lg:px-8 max-w-7xl mx-auto z-50">
-                <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-900 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
-                    <p className="text-sm leading-6 text-white">
+                <div className="pointer-events-auto flex flex-col sm:flex-row items-center justify-between gap-x-6 bg-gray-900 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+                    <div className="text-sm leading-6 text-white">
                         <strong className="font-semibold">Ce site utilise des cookies</strong>
                         <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline h-0.5 w-0.5 fill-current">
                             <circle r={1} cx={1} cy={1} />
@@ -40,7 +40,8 @@ export default function CookieBanner() {
                         <Link href="/politique-de-confidentialite" className="underline hover:text-gray-300">
                             En savoir plus
                         </Link>
-                    </p>
+                        <p className="mt-1 text-xs text-gray-400">Photos et textes actuellement non contractuelles, site en cours de développement.</p>
+                    </div>
                     <button
                         type="button"
                         className="-m-3 flex-none p-3 focus-visible:outline-offset-[-4px]"
